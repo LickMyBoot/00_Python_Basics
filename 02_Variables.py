3d3aa119427fdb09285c5b47a717b6faecfb6842
LickMyBoot <142548465+LickMyBoot@users.noreply.github.com>
2023-11-02 12:19:08 +1300

    Add files via upload

---
+++ 02_Variables.py
@@ -0,0 +1,25 @@
+# code to learn about variables
+
+# variables can be a string
+greeting = "hello world"
+
+# they can also be numbers
+fav_num = 7
+
+# this variable has a problem
+my_name = "Miss G"
+
+num_string = "5"
+
+print(fav_num * 2)
+print(my_name)
+print(greeting)
+
+print(num_string * 5)
+
+# Below is a constant (the name is in UPPERCASE)
+PI = 3.14
+
+num_2 = 2
+
+print(num_2 * 2)
